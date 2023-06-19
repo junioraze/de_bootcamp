b6269bf6be6c622d6674cf316f70f3a92060cf1e
Helmiton Rebouças de Azevedo Junior <junioraze2@gmail.com>
2023-06-19 16:39:40 -0300

    feat: :sparkles: Jupyter notebook with step by step ingestion

--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import sys
 
 def make_papeline():
     '''
     DO SOMETHING FANCY
     '''
-    print('Pipeline executed!')
+    date = sys.argv[1]
+    print(f'Pipeline executed! Date:{date}')
     
 if __name__ == '__main__':
     make_papeline()
